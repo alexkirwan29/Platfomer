1b9ac5d1e1c7e9bdd5fd9c36f47c7f61e1e4b274
Alex Kirwan <alexkirwan29@gmail.com>
2015-12-10 20:40:00 +0800

    started camera transition

---
+++ Game/2DPlatformer/Assets/Scrpts/cameraTransition.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class cameraTransition : MonoBehaviour
+{
+
+    public Camera twoD;
+    public Camera threeD;
+    public bool is3D = false;
+    bool oldIs3D = false;
+
+    void Update()
+    {
+
+        if (is3D != oldIs3D)
+        {
+            oldIs3D = is3D;
+            Debug.Log("3D mode was changed to " + is3D.ToString());
+
+        }
+
+        if (Input.GetButtonDown("swapCamera"))
+        {
+
+            if (is3D = !is3D)
+            {
+                threeD.gameObject.SetActive(false);
+
+            }
+                                                            //at the moment only the 3D camera is changing its state. make it so when you hit "swapCamera" it disables one and enables the other
+            else
+            {
+                threeD.gameObject.SetActive(true);
+            }
+
+        }
+    }
+
+}
---
+++ Game/2DPlatformer/Assets/Scrpts/playerMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+
+    public float speed = 1;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+        if (Input.GetButtonDown("right"));
+        {
+            
+        }
+
+	
+	}
+	
+
+}
